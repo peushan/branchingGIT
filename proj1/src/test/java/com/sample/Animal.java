bd26e1d6ed6766a6f27835da1bc606fd3b62ca69
PamodhP <51316031+PamodhP@users.noreply.github.com>
2021-06-26 18:55:26 +1200

    Update Animal.java
    update pamodh1

--- proj1/src/test/java/com/sample/Animal.java
+++ proj1/src/test/java/com/sample/Animal.java
@@ -2,6 +2,9 @@ package com.sample;
 
 public class Animal {
     public static void main(String[] args) {
+        
         System.out.println("testing");
+        
+        System.out.println("testing2");
     }
 }
